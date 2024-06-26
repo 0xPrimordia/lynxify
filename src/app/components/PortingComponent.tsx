97cdfb40a0945f4669bcfb9305b26efb4eb34938
Kevin Compton <kevin@wecreativeagency.com>
2024-06-26 14:02:59 -0700

    updates

--- src/app/components/PortingComponent.tsx
+++ src/app/components/PortingComponent.tsx
@@ -5,7 +5,13 @@ import {Autocomplete, AutocompleteItem} from "@nextui-org/react";
 import Web3, { ContractAbi } from 'web3';
 import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
 
-
+interface Window {
+    ethereum: {
+        isMetaMask?: boolean;
+        request: (...args: any[]) => Promise<any>;
+        // Add any other properties you expect from window.ethereum here
+    };
+}
 
 type SupportedNetwork = {
     network: Network
@@ -91,18 +97,18 @@ export default function PortingComponent() {
 
     async function connectWallet() {
         console.log("connecting wallet")
-        if (window.ethereum) {
+        //if (window.ethereum) {
             try {
-                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                console.log(accounts)
-                setConnectedWallet(accounts[0])
-                return accounts[0]; // This is the connected MetaMask account
+                //const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                //console.log(accounts)
+                //setConnectedWallet(accounts[0])
+                //return accounts[0]; // This is the connected MetaMask account
             } catch (error) {
                 console.error("Error connecting to MetaMask", error);
             }
-        } else {
-            alert("Please install MetaMask!");
-        }
+        //} else {
+        //    alert("Please install MetaMask!");
+        //}
     }
 
     async function executeContractFunction() {
@@ -124,24 +130,24 @@ export default function PortingComponent() {
 
     async function approve() {
         const fromAddress = await connectWallet();
-        const web3 = new Web3(window.ethereum); // Use MetaMask's provider
+       //const web3 = new Web3(window.ethereum); // Use MetaMask's provider
         if(!approvalStep?.abi) return
-        const contract = new web3.eth.Contract(JSON.parse(approvalStep.abi), approvalStep.target); //approvalStep.target
+        //const contract = new web3.eth.Contract(JSON.parse(approvalStep.abi), approvalStep.target); //approvalStep.target
     
         try {
             let gasEstimate;
             try {
-                gasEstimate = await contract.methods.approve(approvalStep.spender, approvalStep.amount).estimateGas({ from: fromAddress });
+                //gasEstimate = await contract.methods.approve(approvalStep.spender, approvalStep.amount).estimateGas({ from: fromAddress });
             } catch (error) {
                 console.error('Gas estimate failed, using fallback gas limit:', error);
                 gasEstimate = 5000000
                 //return null;
             }    
             console.log(`Sending transaction with gas limit: ${gasEstimate}`);
-            const receipt = await contract.methods.approve(approvalStep.spender, approvalStep.amount)
-                .send({ from: fromAddress, gas: gasEstimate.toString() });
-            console.log('Transaction receipt:', receipt);
-            return receipt.transactionHash;
+            //const receipt = await contract.methods.approve(approvalStep.spender, approvalStep.amount)
+            //    .send({ from: fromAddress, gas: gasEstimate.toString() });
+            //console.log('Transaction receipt:', receipt);
+            //return receipt.transactionHash;
         } catch (error) {
             console.error('Error sending transaction:', error);
             return null;
@@ -165,36 +171,36 @@ export default function PortingComponent() {
 
     async function lock() {
         const fromAddress = await connectWallet();
-        const web3 = new Web3(window.ethereum); // Use MetaMask's provider
+        //const web3 = new Web3(window.ethereum); // Use MetaMask's provider
         if(!lockStep?.abi) return null;
-        const contract = new web3.eth.Contract(JSON.parse(lockStep.abi), lockStep.target);
+        //const contract = new web3.eth.Contract(JSON.parse(lockStep.abi), lockStep.target);
     
         const param1 = "0x128";
         const param2 = lockStep.target;
         const param3 = lockStep.amount;
-        const param4 = web3.utils.asciiToHex("0.0.4372449");
+        //const param4 = web3.utils.asciiToHex("0.0.4372449");
 
         console.log("Method Data Parameters:", {
             param1,
             param2,
             param3,
-            param4
+            
         });
 
         try {
             let gasEstimate;
             try {
-                gasEstimate = await contract.methods.transfer("0x128", selectedAssetId, lockStep.amount, web3.utils.asciiToHex("0.0.4372449")).estimateGas({ from: fromAddress });
+                //gasEstimate = await contract.methods.transfer("0x128", selectedAssetId, lockStep.amount, web3.utils.asciiToHex("0.0.4372449")).estimateGas({ from: fromAddress });
             } catch (error) {
                 console.error('Gas estimate failed, using fallback gas limit:', error);
                 gasEstimate = 5000000
                 //return null;
             }
             console.log(`Sending transaction with gas limit: ${gasEstimate}`);
-            const receipt = await contract.methods.lock("0x128", selectedAssetId, lockStep.amount, web3.utils.asciiToHex("0.0.4372449"))
-                .send({ from: fromAddress, gas: gasEstimate.toString() });
-            console.log('Transaction receipt:', receipt);
-            return receipt.transactionHash;
+            //const receipt = await contract.methods.lock("0x128", selectedAssetId, lockStep.amount, web3.utils.asciiToHex("0.0.4372449"))
+               // .send({ from: fromAddress, gas: gasEstimate.toString() });
+            //console.log('Transaction receipt:', receipt);
+            //return receipt.transactionHash;
         } catch (error) {
             console.error('Error sending transaction:', error);
             return null;
@@ -203,12 +209,12 @@ export default function PortingComponent() {
 
     async function checkMinAmount() {
         const account = await connectWallet();
-        const web3 = new Web3(window.ethereum); // Use MetaMask's provider
+       // const web3 = new Web3(window.ethereum); // Use MetaMask's provider
         if(!selectedAsset) return
         const tokenAddress = selectedAsset?.id; // The contract address of the token
         const tokenDecimals = selectedAsset?.decimals;
-        const networkId = await web3.eth.net.getId();
-        console.log("Connected to network ID:", networkId);
+        //const networkId = await web3.eth.net.getId();
+        //console.log("Connected to network ID:", networkId);
         console.log("Using account address:", account);
         // You need the ABI for the ERC-20 token, which is standard and can be reused
         //const standardErc20Abi = [{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_symbol","type":"string"},{"internalType":"uint8","name":"decimals_","type":"uint8"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_account","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_account","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]
@@ -218,25 +224,25 @@ export default function PortingComponent() {
             // Add other necessary methods here
         ];
 
-        const tokenContract = new web3.eth.Contract(fullErc20Abi, tokenAddress);
-        console.log("Contract setup with ABI and address:", tokenContract);
+        //const tokenContract = new web3.eth.Contract(fullErc20Abi, tokenAddress);
+        //console.log("Contract setup with ABI and address:", tokenContract);
         
         try {
-            const balance = await tokenContract.methods.balanceOf(account).call();
-            const balanceAdjusted = Number(balance) / (10 ** tokenDecimals); // Adjust balance based on token decimals
+            //const balance = await tokenContract.methods.balanceOf(account).call();
+            //const balanceAdjusted = Number(balance) / (10 ** tokenDecimals); // Adjust balance based on token decimals
 
             const minAmountAdjusted = Number(selectedAsset.minAmount) / (10 ** tokenDecimals); // Adjust minAmount based on token decimals
 
-            console.log(`Fetched balance: ${balance} raw, ${balanceAdjusted} adjusted`);
+            //console.log(`Fetched balance: ${balance} raw, ${balanceAdjusted} adjusted`);
             console.log(`Required minimum amount: ${selectedAsset.minAmount} raw, ${minAmountAdjusted} adjusted`);
 
-            if (balanceAdjusted >= minAmountAdjusted) {
-                console.log("Sufficient funds:", balanceAdjusted);
-                return true;
-            } else {
-                console.log("Insufficient funds:", balanceAdjusted);
-                return false;
-            }
+            //if (balanceAdjusted >= minAmountAdjusted) {
+           //     console.log("Sufficient funds:", balanceAdjusted);
+            //    return true;
+            //} else {
+            //    console.log("Insufficient funds:", balanceAdjusted);
+            //    return false;
+            //}
         } catch (error) {
             console.error("Error checking token balance:", error);
             return false;
--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -2,31 +2,15 @@
 import React, { useState, useEffect } from "react";
 //import { createHashPackSigner, useHashConnect, HashportClientProviderWithRainbowKit } from "@hashport/react-client";
 //import "../utils/polyfills"
-import { usePrivy } from "@privy-io/react-auth";
-import { useWallets } from '@privy-io/react-auth';
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-    const { ready, authenticated } = usePrivy();
-    const { wallets} = useWallets();
-    //@ts-ignore
-    //const { hashConnect, pairingData } = useHashConnect({ mode: 'testnet' });
-    //const hederaSigner = pairingData && createHashPackSigner(hashConnect, pairingData);
-    
-    const router = useRouter();
-    const embeddedWallet = wallets.find((wallet) => wallet.walletClientType === 'privy');
-
-    if (!ready) return null;
-    if (ready && !authenticated) {
-        router.push("/");
-    }
 
     return (
         
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
             <h2>Dashboard</h2>
-            <p>Privy Wallet: {embeddedWallet?.address}</p>
 
         </div>
         
