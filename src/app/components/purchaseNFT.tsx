9803d8c15552d5f49f4ce2d1f6aa5360e3bb8079
Kevin Compton <kevin@wecreativeagency.com>
2024-11-23 13:55:16 -0600

    Update purchaseNFT.tsx

--- src/app/components/purchaseNFT.tsx
+++ src/app/components/purchaseNFT.tsx
@@ -1,4 +1,4 @@
-import { ContractExecuteTransaction, Hbar, ContractId } from "@hashgraph/sdk";
+import { ContractExecuteTransaction, Hbar, ContractId, TransactionId } from "@hashgraph/sdk";
 import { Button } from "@nextui-org/react";
 import { useWalletContext } from "../hooks/useWallet";
 import { useState } from "react";
@@ -21,7 +21,8 @@ function PurchaseNFT({ apiUrl, tokenId }: { apiUrl: string, tokenId: string }) {
                 .setContractId(ContractId.fromString(contractAddress))
                 .setGas(400000)
                 .setPayableAmount(new Hbar(300))
-                .setFunction("purchaseNFT");
+                .setFunction("purchaseNFT")
+                .setTransactionId(TransactionId.generate(account));
 
             // Convert to base64 string for wallet connect
             const base64Tx = transactionToBase64String(transaction);
