9f417917b3171172da4a82c060ae1b0c8e7eb06b
Kevin Compton <kevin@wecreativeagency.com>
2024-12-11 13:27:15 -0600

    update

--- src/app/components/FeedbackForm.tsx
+++ src/app/components/FeedbackForm.tsx
@@ -30,7 +30,7 @@ const FeedbackForm = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('/', {
+      const response = await fetch('/__forms.html', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams({
