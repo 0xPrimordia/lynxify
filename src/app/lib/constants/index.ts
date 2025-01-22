322217840e4e29abda1267b38a9c1439dc9ef2b1
Kevin Compton <kevin@wecreativeagency.com>
2025-01-22 17:38:04 -0600

    update

--- src/app/lib/constants/index.ts
+++ src/app/lib/constants/index.ts
@@ -1,3 +1,3 @@
-export const WHBAR_ID = '0.0.15058';
+export const WHBAR_ID = process.env.NEXT_PUBLIC_WHBAR_HEDERA_ID || '0.0.15058'; // fallback to testnet for local dev
 export const QUOTER_V2_ADDRESS = process.env.NEXT_PUBLIC_QUOTER_V2_ADDRESS as string;
 export const SWAP_ROUTER_ADDRESS = process.env.NEXT_PUBLIC_SWAP_ROUTER_ADDRESS as string; 
