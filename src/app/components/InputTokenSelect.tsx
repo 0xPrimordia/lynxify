c4bf9924e165b226fdf8a831cedb7269bf4d4499
Kevin Compton <kevin@wecreativeagency.com>
2025-01-22 22:12:32 -0600

    Update InputTokenSelect.tsx

--- src/app/components/InputTokenSelect.tsx
+++ src/app/components/InputTokenSelect.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { Token } from "../hooks/useTokens";
+import { Token } from "@/app/types";
 import { Chip, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
